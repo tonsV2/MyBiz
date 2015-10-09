74a72d1f3c45a08e5508487ca2f14c1a43be0bc1
Sebastian Jensen <sebastianthegreatful@gmail.com>
2015-10-09 20:39:15 +0200

    Stripped useless links.

--- src/main/java/dk/fitfit/mybiz/assembler/ExpenseResourceAssembler.java
+++ src/main/java/dk/fitfit/mybiz/assembler/ExpenseResourceAssembler.java
@@ -9,14 +9,12 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 public class ExpenseResourceAssembler {
+	// TODO: don't depend on Expense
 	public static ExpenseResource from(final Expense expense) {
 		final ExpenseResource.Builder builder = new ExpenseResource.Builder();
 		final long id = expense.getId();
 		final ExpenseResource resource = builder.from(expense).build();
 		resource.add(linkTo(methodOn(ExpenseController.class).findOne(id)).withSelfRel());
-		resource.add(linkTo(methodOn(ExpenseController.class).delete(id)).withRel("delete"));
-		resource.add(linkTo(methodOn(ExpenseController.class).update(expense, id)).withRel("put"));
-		resource.add(linkTo(methodOn(ExpenseController.class).findAll()).withRel("list"));
 		return resource;
 	}
 }
